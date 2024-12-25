c9ad5706b8c9e2121ce69d9efd7b34ac7c990624
Anders <abl@optoceutics.com>
2024-12-25 20:00:07 +0100

    type hinting backwards compatibility

--- globalite/connection_manager.py
+++ globalite/connection_manager.py
@@ -1,5 +1,8 @@
-import sqlite3
+from __future__ import annotations
 from typing import Optional
+import sqlite3
+
+
 
 class _ConnectionManager:
 
@@ -10,6 +13,6 @@ class _ConnectionManager:
     def __enter__(self) -> tuple[sqlite3.Connection, sqlite3.Cursor]:
         self._conn = sqlite3.connect(self._globalite.db_file)
         return self._conn, self._conn.cursor()
-    
+
     def __exit__(self, *_):
         self._conn.close()
